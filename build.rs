d3e34047bd9fc0deebe255db28fee387e2632bfe
Jade Ellis <jade@ellis.link>
2024-04-19 01:29:28 +0100

    Formatting

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    #[cfg(target_os="windows")]
+    #[cfg(target_os = "windows")]
     {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("assets/icon.ico");
